eb5e8a335236f75e6e4f1b2ff39fedaac2b99059
slabwu <134193743+slabwu@users.noreply.github.com>
2023-05-30 07:58:03 +0700

    Add function getPlayerChoice

--- script.js
+++ script.js
@@ -2,15 +2,31 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random()*3) + 1;
     switch (choice) {
         case 1:
-            return 'Rock';
+            return 'rock';
             break;
         case 2:
-            return 'Paper';
+            return 'paper';
             break;
         case 3:
-            return 'Scissors';
+            return 'scissors';
             break;
     }
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice () {
+    let choice = prompt('Welcome to "Rock, Paper, Scissors"! What\'s your move?')
+    const validMoves = ['rock', 'paper', 'scissors'];
+    console.log(validMoves);
+    if (validMoves.includes(choice.toLowerCase())) {
+        return choice.toLowerCase();
+    } else {
+        alert("That's not a valid move! Try again.");
+        getPlayerChoice();
+    }
+}
+
+let computerSelection = getComputerChoice();
+let playerSelection = getPlayerChoice();
+
+console.log(computerSelection);
+console.log(playerSelection);
