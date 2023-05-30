decb9bc0278d906da2817ad46504bd25ad7190d7
slabwu <134193743+slabwu@users.noreply.github.com>
2023-05-30 08:48:45 +0700

    Delete confirm() & set game to five rounds

--- script.js
+++ script.js
@@ -13,14 +13,16 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let choice = prompt('Welcome to "Rock, Paper, Scissors"! What\'s your move?');
+function getPlayerChoice(round) {
+    let choice = (round === 1) ? 
+        prompt('Welcome to round 1 of "Rock, Paper, Scissors"! What\'s your move?'): 
+        prompt(`Round ${round}: What's your move?`); 
     const validMoves = ['rock', 'paper', 'scissors'];
     if (choice && validMoves.includes(choice.toLowerCase())) {
         return choice.toLowerCase();
     } else {
         alert('That\'s not a valid move! Try again.');
-        getPlayerChoice();
+        getPlayerChoice(round);
     }
 }
 
@@ -30,36 +32,48 @@ function capitalize(string) {
     return string.replace(letter, letter.toUpperCase());
 }
 
-function game(computerSelection, playerSelection) {
+function play(computerSelection, playerSelection) {
     alert(`The opponent plays ${computerSelection}.`);
     if (computerSelection === playerSelection) {
         alert('It\'s a tie!');
+        return 'tie';
     } else {
         switch (playerSelection) {
         case 'rock':
             if (computerSelection === 'scissors') {
                 alert(`You win! ${capitalize(playerSelection)} beats ${computerSelection}.`); 
+                return 'win';
             } else {
-                alert(`You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`);           
+                alert(`You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`);   
+                return 'loss';        
             }
             break;
         case 'paper':
             if (computerSelection === 'rock') {
                 alert(`You win! ${capitalize(playerSelection)} beats ${computerSelection}.`); 
+                return 'win';
             } else {
-                alert(`You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`);           
+                alert(`You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`);   
+                return 'loss';        
             }
             break;
         case 'scissors':
             if (computerSelection === 'paper') {
-                alert(`You win! ${capitalize(playerSelection)} beats ${computerSelection}.`); 
+                alert(`You win! ${capitalize(playerSelection)} beats ${computerSelection}.`);
+                return 'win';
             } else {
-                alert(`You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`);           
+                alert(`You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`);
+                return 'loss';           
             }
             break;
         }
     }
-    confirm('Play again?') ? game(getComputerChoice(), getPlayerChoice()): null;
 }
 
-game(getComputerChoice(), getPlayerChoice());
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        play(getComputerChoice(), getPlayerChoice(i));
+    }
+}
+
+game();
